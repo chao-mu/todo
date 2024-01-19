d4a1a9de77687dadcab83c24e581448dbc4f1dbe
Danimal <danimal@hackpoetic.com>
2024-01-19 13:17:17 -0500

    feat: support popping multiple indexes

--- todo.py
+++ todo.py
@@ -6,58 +6,67 @@ FILE_NAME = "TODO"
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("command", help="Command to run", choices=["add", "list", "pop", "shift"], default="list", nargs="?")
-    parser.add_argument("task", help="Task to add/remove", nargs="?")
-    args = parser.parse_args()
+    subparsers = parser.add_subparsers(help="Command to run", dest="command")
+    subparsers.default = "list"
 
-    task = args.task
-    if args.task is not None and args.command in ["pop", "shift"]:
-        task = int(task)
+    add_parser = subparsers.add_parser("add", help="Add a task")
+    add_parser.add_argument("task", help="Task to add")
 
+    subparsers.add_parser("list", help="List tasks")
 
-    if args.command == "add":
-        add(args.task)
-    elif args.command == "list":
-        list()
-    elif args.command == "pop":
-        pop(task)
-    elif args.command == "shift":
-        shift(task)
+    pop_parser = subparsers.add_parser("pop", help="Pop tasks")
+    pop_parser.add_argument("tasks", help="Task to pop", type=int, nargs="*")
+
+    shift_parser = subparsers.add_parser("shift", help="Shift tasks")
+    shift_parser.add_argument("tasks", help="Task to shift", type=int, nargs="*")
+
+    args = vars(parser.parse_args())
+    command = args.pop("command")
+
+    if command == "add":
+        cmd_add(**args)
+    elif command == "list":
+        cmd_list(**args)
+    elif command == "pop":
+        cmd_pop(**args)
+    elif command == "shift":
+        cmd_shift(**args)
     else:
         print("Invalid command")
 
-def add(task):
+def cmd_add(task):
     with open(FILE_NAME, "a") as f:
         f.write(task + "\n")
 
-def list():
+def cmd_list():
     with open(FILE_NAME, "r") as f:
         for idx, line in enumerate(f):
             task = line.strip()
             print(f'{idx} {task}')
 
-def shift(task_idx):
-    if task_idx is None:
-        task_idx = 0
+def cmd_shift(tasks):
+    if not tasks:
+        tasks = [0]
 
-    pop(task_idx)
+    cmd_pop(tasks)
 
-
-def pop(task_idx):
+def cmd_pop(tasks):
     with open(FILE_NAME, "r") as f:
         lines = f.readlines()
 
-    if task_idx is None:
-        task_idx = len(lines) - 1
+    if not tasks:
+        tasks = [len(lines) - 1]
     
-    if len(lines) <= task_idx or task_idx < 0:
-        print("Invalid task index")
+    if any(task_idx >= len(lines) or task_idx < 0 for task_idx in tasks):
+        print("Invalid task index listed")
         return
 
-    task = lines[task_idx].strip()
-    print(task)
-
-    kept = lines[:task_idx] + lines[task_idx+1:]
+    kept = []
+    for idx, line in enumerate(lines):
+        if idx not in tasks:
+            kept.append(line)
+        else:
+            print(line.strip())
 
     with open(FILE_NAME, "w") as f:
         f.write("".join(kept))
